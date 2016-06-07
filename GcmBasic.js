0be3c6b59ccfebeca261ee3cde6ccea2ff29802a
Jens Oknelid <jens.oknelid@gmail.com>
2016-06-07 14:19:39 +0200

    Made it build

---
+++ GcmBasic.js
@@ -0,0 +1,56 @@
+'use strict';
+
+import {
+  NativeModules,
+  DeviceEventEmitter,
+} from 'react-native';
+
+var GcmNative = NativeModules.GcmModule;
+var _notifHandlers = new Map();
+
+var DEVICE_NOTIF_EVENT = 'remoteNotificationReceived';
+var NOTIF_REGISTER_EVENT = 'remoteNotificationsRegistered';
+
+class GcmBasic {
+  
+//  static launchNotification = GcmNative.launchNotification ? JSON.parse(GcmNative.launchNotification) : undefined;
+  static launchNotification = {};
+
+  static addEventListener(type: string, handler: Function) {
+    var listener;
+    if (type === 'register') {
+      listener = DeviceEventEmitter.addListener(
+        NOTIF_REGISTER_EVENT,
+        (registrationInfo) => {
+          handler(registrationInfo.deviceToken);
+        }
+      );
+    }
+    _notifHandlers.set(handler, listener);
+  }
+
+  static subscribeTopic(token, topic) {
+    GcmNative.subscribeTopic(token, topic);
+  }
+
+  static requestPermissions() {
+    GcmNative.requestPermissions();
+  }
+
+  static removeEventListener(type: string, handler: Function) {
+    var listener = _notifHandlers.get(handler);
+    if (!listener) {
+      return;
+    }
+    listener.remove();
+    _notifHandlers.delete(handler);
+  }
+}
+
+/*
+if (GcmBasic.launchNotification) {
+  GcmBasic.launchNotification = JSON.parse(GcmBasic.launchNotification);
+}
+*/
+
+module.exports = GcmBasic;
