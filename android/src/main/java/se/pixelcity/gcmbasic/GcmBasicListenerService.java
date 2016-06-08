0390d2d372eec1a52afeaaf97185cea13355518c
Jens Oknelid <jens.oknelid@gmail.com>
2016-06-08 13:15:18 +0200

    Sending notification from service

--- android/src/main/java/se/pixelcity/gcmbasic/GcmBasicListenerService.java
+++ android/src/main/java/se/pixelcity/gcmbasic/GcmBasicListenerService.java
@@ -1,16 +1,19 @@
 
 package se.pixelcity.gcmbasic;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GcmListenerService;
-
-import org.json.*;
-
-import java.util.Set;
 
 public class GcmBasicListenerService extends GcmListenerService {
 
@@ -18,7 +21,7 @@ public class GcmBasicListenerService extends GcmListenerService {
     private static final String TITLE_KEY = "gcm.notification.title";
     private static final String BODY_KEY = "gcm.notification.body";
 
-    static boolean mAppActive;
+    static boolean mAppActive = false;
     public static void setAppActive(boolean active) {
         mAppActive = active;
         Log.d(TAG, "Setting appActive: " + active);
@@ -57,24 +60,45 @@ public class GcmBasicListenerService extends GcmListenerService {
         if (data.containsKey(TITLE_KEY) && data.containsKey(BODY_KEY)) {
             Log.d(TAG, "sendNotification - displaying notification");
 
-//            Intent intent = new Intent(this, MainActivity.class);
-//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-//                PendingIntent.FLAG_ONE_SHOT);
-//
-//        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-//                .setSmallIcon(R.drawable.ic_stat_ic_notification)
-//                .setContentTitle("GCM Message")
-//                .setContentText(message)
-//                .setAutoCancel(true)
-//                .setSound(defaultSoundUri)
-//                .setContentIntent(pendingIntent);
-//
-//        NotificationManager notificationManager =
-//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-//
-//        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+            String packageName = getApplication().getPackageName();
+            PackageManager packageManager = getApplicationContext().getPackageManager();
+            Intent launchIntent = packageManager.getLaunchIntentForPackage(packageName);
+            String className = launchIntent.getComponent().getClassName();
+
+            Intent intent;
+            PendingIntent pendingIntent;
+            try {
+                intent = new Intent(this, Class.forName(className));
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent, PendingIntent.FLAG_ONE_SHOT);
+            }
+            catch (ClassNotFoundException e) {
+                Log.e(TAG, "sendNotification - activity not found");
+                return;
+            }
+
+            ApplicationInfo applicationInfo = null;
+            try {
+                applicationInfo = packageManager.getApplicationInfo(launchIntent.getPackage(), PackageManager.GET_META_DATA);
+            }
+            catch (PackageManager.NameNotFoundException e) {
+                Log.e(TAG, "sendNotification - package not found");
+                return;
+            }
+
+            Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(applicationInfo.icon)
+                .setContentTitle(data.getString(TITLE_KEY))
+                .setContentText(data.getString(BODY_KEY))
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+                .setExtras(data);
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
         }
         else {
             Log.e(TAG, "sendNotification - unrecognized message");
